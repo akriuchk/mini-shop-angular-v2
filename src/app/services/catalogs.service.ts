55f0fb8e80d5569ec990ca4789eb799d6a8997a5
alexkruk <alexkruk93@yandex.ru>
2019-11-02 03:32:45 +0300

    change build script

--- src/app/services/catalogs.service.ts
+++ src/app/services/catalogs.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Catalog } from '../model/catalog';
 import { environment } from 'src/environments/environment';
 
 @Injectable()
@@ -19,9 +18,4 @@ export class CatalogsService {
       observe: 'response'
     });
   }
-
-  // public findAll(): Observable<Catalog[]> {
-  //   return this.http.get<Catalog[]>(this.bedsheetsUrl)
-  //   ;
-  // }
 }
